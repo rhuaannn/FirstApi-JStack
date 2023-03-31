fcfe857e97d9b600abccc715571d934654141a9f
rhuaannn <rhuanmarques1223@gmail.com>
2023-03-30 22:10:46 -0300

    create api => finish

--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,26 @@ module.exports = [
   {
     endpoint: '/users',
     method: 'GET',
-    handler: UserController.listUsers,    
+    handler: UserController.listUsers,
   },
-
   {
     endpoint: '/users/:id',
     method: 'GET',
-    handler: UserController.getUserById,    
+    handler: UserController.getUserById,
   },
   {
     endpoint: '/users',
     method: 'POST',
-    handler: UserController.createUser,    
+    handler: UserController.createUser,
   },
-];
-
-
+  {
+    endpoint: '/users/:id',
+    method: 'PUT',
+    handler: UserController.updateUser,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'DELETE',
+    handler: UserController.deleteUser,
+  },
+]
